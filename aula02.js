5ec588532929f3cd2aad5733a9459fe8a78ae5a2
André Luiz Melicio <71500309+Coffee4-4@users.noreply.github.com>
2021-12-22 12:51:58 -0300

    update aula 02

--- aula02.js
+++ aula02.js
@@ -3,9 +3,19 @@ console.log("Travalhando com variáveis.")
 //JS é case sensitive
 
 
-//temos duas variaveis diferentes declaradas, com o mesmo nome, apesar do nome identico são espaços diferentes na memoria 
+//temos duas variaveis diferentes declaradas, com o mesmo nome, apesar do nome identico são espaços diferentes na memoria
 idade = 27;
 Idade = 30;
 
+//Ambos são formas de reservar espaço na memoria
+const nome = "André";
+
+let sobrenome="Melicio";
+
 console.log(idade);
 console.log(Idade);
+
+console.log("Operações Aritiméticas")
+
+console.log(2+2);
+console.log(2+2*3);
