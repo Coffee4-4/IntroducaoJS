3601cd00f1a60176cf111a8846360bfa0a6a4a5f
André Luiz Melicio <71500309+Coffee4-4@users.noreply.github.com>
2021-12-22 11:02:49 -0300

    Initial commit

---
+++ aula01.js
@@ -0,0 +1,9 @@
+console.log("Olá Mundo!");
+
+const idade = 27;
+
+console.log(idade);
+console.log(idade + 2);
+console.log(idade - 2);
+console.log(idade / 2);
+console.log(idade * 2);
---
+++ aula02.js
@@ -0,0 +1,11 @@
+console.log("Travalhando com variáveis.")
+
+//JS é case sensitive
+
+
+//temos duas variaveis diferentes declaradas, com o mesmo nome, apesar do nome identico são espaços diferentes na memoria 
+idade = 27;
+Idade = 30;
+
+console.log(idade);
+console.log(Idade);
